5ad93d90642138d2cc1e881093765ba4bf58f391
bigrushdog <randall.rushing@gmail.com>
2017-09-12 14:14:35 +0800

    Compatibility: Add api to take screenshots to CrystalUtils
    abc: extracted from DUI
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/com/android/internal/crystal/util/CrystalUtils.java
+++ core/java/com/android/internal/crystal/util/CrystalUtils.java
@@ -17,18 +17,23 @@
 package com.android.internal.crystal.util;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
-
 import com.android.internal.statusbar.IStatusBarService;
+
+import java.util.List;
 
 /**
  * Some custom utilities
  */
 public class CrystalUtils {
+
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
 
     public static void switchScreenOff(Context ctx) {
         PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
@@ -43,6 +48,20 @@ public class CrystalUtils {
 
     public static void toggleCameraFlash() {
         FireActions.toggleCameraFlash();
+    }
+
+    public static void takeScreenshot(boolean full) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ie) {
+            // Do nothing
+        }
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     private static final class FireActions {
