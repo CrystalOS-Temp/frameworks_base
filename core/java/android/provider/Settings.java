bbc3654ebb1dd91fdc4e58f813191cc3d4556c44
ezio84 <brabus84@gmail.com>
2020-11-15 09:45:01 +0100

    Less boring heads up option [1/2]
    Allow to show heads up only for dialer and messaging
    app when they are not in foreground
    
    Change-Id: I9ce517a5974b1567339bc7d7c84ee35bb89c774b
    
    ---
    
    Less boring headsup: allow any sms/dialer app set as default by user
    thanks GeoZac for the suggestion!
    
    Change-Id: Ie7a725bd0fc579462b736f2e8520d59d14f402f2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5219,6 +5219,20 @@ public final class Settings {
         public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
 
         /**
+         * Wheter to play notification sound and vibration if screen is ON
+         * 0 - never
+         * 1 - always
+         * @hide
+         */
+        public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
+
+        /**
+         * Whether to show heads up only for dialer and sms apps
+         * @hide
+         */
+        public static final String LESS_BORING_HEADS_UP = "less_boring_heads_up";
+
+        /**
          * Double tap on lockscreen to sleep
          * @hide
          */
