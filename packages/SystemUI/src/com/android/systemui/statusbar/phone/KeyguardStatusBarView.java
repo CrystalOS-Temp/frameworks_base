10cbf1051437282f2da8326e72e906da0c233991
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2021-03-20 10:35:14 +0000

    SystemUI: Fix status bar / quick settings margins and paddings
    - Change keyguard_carrier_text_margin into a padding
    - Link keyguard paddings to status_bar_padding_{start|end}
    - Add status_bar_padding_{start|end} to quick settings header
    - Remove several unnecessary margins and paddings
    
    Change-Id: Ic91fa398813e1907297bb0892c444d96405950e7

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -158,20 +158,16 @@ public class KeyguardStatusBarView extends RelativeLayout implements
         mMultiUserAvatar.setLayoutParams(lp);
 
         // System icons
-        updateSystemIconsLayoutParams();
+        lp = (MarginLayoutParams) mSystemIconsContainer.getLayoutParams();
+        lp.setMarginStart(getResources().getDimensionPixelSize(
+                R.dimen.system_icons_super_container_margin_start));
+        mSystemIconsContainer.setLayoutParams(lp);
 
         // Respect font size setting.
         mCarrierLabel.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                 getResources().getDimensionPixelSize(
                         com.android.internal.R.dimen.text_size_small_material));
-        lp = (MarginLayoutParams) mCarrierLabel.getLayoutParams();
 
-        int marginStart = calculateMargin(
-                getResources().getDimensionPixelSize(R.dimen.keyguard_carrier_text_margin),
-                mPadding.first);
-        lp.setMarginStart(marginStart);
-
-        mCarrierLabel.setLayoutParams(lp);
         updateKeyguardStatusBarHeight();
     }
 
