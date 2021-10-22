2eb2f8192216f846ec9147d00c6b79f4002b6f25
xyyx <xyyx@mail.ru>
2021-10-22 21:03:43 +0200

    LockScreen: Unlock keystore with fingerprint after reboot [1/2]
    Change-Id: I9a8f957445b808ada2669c5a5a4860578ca83aa8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5200,6 +5200,12 @@ public final class Settings {
         public static final String MULTI_AUDIO_FOCUS_ENABLED = "multi_audio_focus_enabled";
 
         /**
+         * Unlock keystore with fingerprint after reboot
+         * @hide 
+         */
+        public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -5326,6 +5332,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE_VENDOR_HINT);
+            PRIVATE_SETTINGS.add(FP_UNLOCK_KEYSTORE);
         }
 
         /**
