1b0a896e315d7973aa8d74dff47e99a5f7383aa5
bencetari <bence.tari@gmail.com>
2021-10-25 22:11:30 +0200

    Revert "Add support for app signature spoofing"
    This reverts commit 3ee992c0d2103877c37b662d3eafa994698595b3.
    
    Change-Id: I17b994beb8eb2ba73b0c19ad5836926a62faf576

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -3331,9 +3331,8 @@ public class PackageManagerService extends IPackageManager.Stub
                         || ArrayUtils.isEmpty(p.getRequestedPermissions())) ? Collections.emptySet()
                         : mPermissionManager.getGrantedPermissions(ps.name, userId);
 
-                PackageInfo packageInfo = mayFakeSignature(p, PackageInfoUtils.generate(p, gids, flags,
-                    ps.firstInstallTime, ps.lastUpdateTime, permissions, state, userId, ps),
-                    permissions);
+                PackageInfo packageInfo = PackageInfoUtils.generate(p, gids, flags,
+                        ps.firstInstallTime, ps.lastUpdateTime, permissions, state, userId, ps);
 
                 if (packageInfo == null) {
                     return null;
@@ -8533,24 +8532,6 @@ public class PackageManagerService extends IPackageManager.Stub
 
     private PackageInfo generatePackageInfo(PackageSetting ps, int flags, int userId) {
         return mComputer.generatePackageInfo(ps, flags, userId);
-    }
-
-    private PackageInfo mayFakeSignature(AndroidPackage p, PackageInfo pi,
-            Set<String> permissions) {
-        try {
-            if (permissions.contains("android.permission.FAKE_PACKAGE_SIGNATURE")
-                    && p.getTargetSdkVersion() > Build.VERSION_CODES.LOLLIPOP_MR1
-                    && p.getMetaData() != null) {
-                String sig = p.getMetaData().getString("fake-signature");
-                if (sig != null) {
-                    pi.signatures = new Signature[] {new Signature(sig)};
-                }
-            }
-        } catch (Throwable t) {
-            // We should never die because of any failures, this is system code!
-            Log.w("PackageManagerService.FAKE_PACKAGE_SIGNATURE", t);
-        }
-        return pi;
     }
 
     @Override
