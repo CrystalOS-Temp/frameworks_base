7db7cc1c205ee44817199f22be3702abb4229836
SKULSHADY <anushekprasal@gmail.com>
2021-11-12 16:30:12 +0100

    Add toggle to disable wired charging animation [1/2]
    Change-Id: Ie8157b9cd1801bf7d81ef03624063f3131e4aa21

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5315,6 +5315,12 @@ public final class Settings {
          */
         public static final String USE_OLD_MOBILETYPE = "use_old_mobiletype"; 
 
+        /**
+         * Whether to show charging animation
+         * @hide
+         */
+        public static final String CHARGING_ANIMATION = "charging_animation";
+
         //ADD_STUFF_ABOVE_ME
         /**
          * Keys we no longer back up under the current schema, but want to continue to
