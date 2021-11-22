e4b81f5d42d76425305583b7b518a19eaa1f07ca
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-11-22 12:24:19 +0100

    Send broadcast when going to sleep

---
+++ core/java/com/android/internal/crystal/util/Intent.java
@@ -0,0 +1,15 @@
+package com.android.internal.crystal.util;
+import android.Manifest;
+
+/**
+ * crDroid specific intent definition class.
+ */
+public class Intent {
+
+    /**
+     * Broadcast action: go to sleep
+     * @hide
+     */
+    public static final String ACTION_GO_TO_SLEEP =
+            "crystal.intent.action.GO_TO_SLEEP";
+}
