8b4f17bb164100d8d4ed6c30aafe45012afb99a4
Pranav Vashi <neobuddy89@gmail.com>
2022-01-21 18:36:33 +0530

    GamingMode: Fix NPE in ActivityManager service
    Reported and Suggested by @Stallix
    
    Log:
    E AndroidRuntime: *** FATAL EXCEPTION IN SYSTEM PROCESS: PackageManager
    01-21 17:23:50.115  2794  2843 E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'void com.android.internal.util.GamingModeHelper.onPackageUninstalled(java.lang.String)' on a null object reference
    01-21 17:23:50.115  2794  2843 E AndroidRuntime:  at com.android.server.wm.ActivityTaskManagerService$LocalService.onPackageUninstalled(ActivityTaskManagerService.java:5566)
    01-21 17:23:50.115  2794  2843 E AndroidRuntime:  at com.android.server.am.ActivityManagerService.broadcastIntentLocked(ActivityManagerService.java:13068)
    01-21 17:23:50.115  2794  2843 E AndroidRuntime:  at com.android.server.am.ActivityManagerService$LocalService.broadcastIntent(ActivityManagerService.java:15849)
    01-21 17:23:50.115  2794  2843 E AndroidRuntime:  at com.android.server.pm.PackageManagerService.doSendBroadcast(PackageManagerService.java:15859)
    01-21 17:23:50.115  2794  2843 E AndroidRuntime:  at com.android.server.pm.PackageManagerService.lambda$sendPackageBroadcast$41$PackageManagerService(PackageManagerService.java:15754)
    01-21 17:23:50.115  2794  2843 E AndroidRuntime:  at com.android.server.pm.PackageManagerService$$ExternalSyntheticLambda61.run(Unknown Source:22)
    01-21 17:23:50.115  2794  2843 E AndroidRuntime:  at android.os.Handler.handleCallback(Handler.java:938)
    01-21 17:23:50.115  2794  2843 E AndroidRuntime:  at android.os.Handler.dispatchMessage(Handler.java:99)
    01-21 17:23:50.115  2794  2843 E AndroidRuntime:  at android.os.Looper.loopOnce(Looper.java:201)
    01-21 17:23:50.115  2794  2843 E AndroidRuntime:  at android.os.Looper.loop(Looper.java:288)
    01-21 17:23:50.115  2794  2843 E AndroidRuntime:  at android.os.HandlerThread.run(HandlerThread.java:67)
    01-21 17:23:50.115  2794  2843 E AndroidRuntime:  at com.android.server.ServiceThread.run(ServiceThread.java:44)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -5727,7 +5727,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
                 mAppWarnings.onPackageUninstalled(name);
                 mCompatModePackages.handlePackageUninstalledLocked(name);
                 mPackageConfigPersister.onPackageUninstall(name);
-                mGamingModeHelper.onPackageUninstalled(name);
+                if (mGamingModeHelper != null) {
+                    mGamingModeHelper.onPackageUninstalled(name);
+                }
             }
         }
 
