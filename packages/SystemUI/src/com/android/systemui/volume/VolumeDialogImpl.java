576de494d98a9215d01ece2d6c91ba243e0888de
Arian <arian.kulmer@web.de>
2022-01-30 22:30:12 +0100

    VolumeDialogImpl: Don't hide the default stream when adjusting the music stream
    If there are two visible rows and the user touches the default (music)
    row, the other row disappears immediately. Avoid this behaviour by
    tracking the row with which this panel was created and keep showing
    that row if the user adjusts the music stream.
    
    Test: While receiving a call both rows are touchable and the additional
          row does not disappear when adjusting the music stream.
    Change-Id: I4e14a0ea50c5cc41cb279c6fbfc8a7d6e6d1ba61
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -272,6 +272,9 @@ public class VolumeDialogImpl implements VolumeDialog,
     private Consumer<Boolean> mCrossWindowBlurEnabledListener;
     private BackgroundBlurDrawable mDialogRowsViewBackground;
 
+    // Variable to track the default row with which the panel is initially shown
+    private VolumeRow mDefaultRow = null;
+
     private final SettingsObserver mSettingsObserver = new SettingsObserver();
     private class SettingsObserver extends ContentObserver {
         SettingsObserver() {
@@ -1413,6 +1416,10 @@ public class VolumeDialogImpl implements VolumeDialog,
             mConfigChanged = false;
         }
 
+        if (mDefaultRow == null) {
+            mDefaultRow = getActiveRow();
+        }
+
         initSettingsH();
         initAppVolumeH();
         mShowing = true;
@@ -1481,6 +1488,7 @@ public class VolumeDialogImpl implements VolumeDialog,
                 .withEndAction(() -> mHandler.postDelayed(() -> {
                     mDialog.dismiss();
                     tryToRemoveCaptionsTooltip();
+                    mDefaultRow = null;
                     mIsAnimatingDismiss = false;
 
                     hideRingerDrawer();
@@ -1524,11 +1532,14 @@ public class VolumeDialogImpl implements VolumeDialog,
                 return true;
             }
 
-            if (row.defaultStream) {
+            // if the row is the default stream or the row with which this panel was created,
+            // show it additonally to the active row if it is one of the following streams
+            if (row.defaultStream || mDefaultRow == row) {
                 return activeRow.stream == STREAM_RING
                         || activeRow.stream == STREAM_NOTIFICATION
                         || activeRow.stream == STREAM_ALARM
                         || activeRow.stream == STREAM_VOICE_CALL
+                        || activeRow.stream == STREAM_MUSIC
                         || activeRow.stream == STREAM_ACCESSIBILITY
                         || mDynamic.get(activeRow.stream);
             }
