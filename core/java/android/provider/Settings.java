e3bffa3c9d1fb7537baec2fc1a5b3cdcdc3212f9
Joey Huab <joey@evolution-x.org>
2022-02-10 22:48:17 +0900

    core: User toggle for streaming apps [1/2]
    * Make it optional as it could break higher quality on some devices.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5580,6 +5580,12 @@ public final class Settings {
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
 
 		/**
+         * Whether to enable spoofing for streaming apps
+         * @hide
+         */
+        public static final String USE_STREAM_SPOOF = "use_stream_spoof";
+		
+		/**
          * Force full screen for devices with cutout
          * @hide
          */
